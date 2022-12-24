3cd3da48352c0eacd6e04bd98e4e05a4b537360e
evgesnia <evgesha-semenova@bk.ru>
2022-12-24 07:37:30 +0400

    finish

---
+++ lab5/calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+while (true)
+{
+    Calculator calc = new Calculator();
+
+        var n = "";
+        while (true)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            Console.WriteLine(calc.Screen);
+            Console.Write(n);
+            n = Console.ReadKey(true).KeyChar.ToString(CultureInfo.InvariantCulture);
+            calc.PressKey(n);
+        }
+}
